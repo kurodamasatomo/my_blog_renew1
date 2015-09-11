30769cca18a5819c69aaa950bfe9b9e2470b176e
黒田 将智 <kurodamasatomo@kurox.local>
2015-09-11 21:26:47 +0900

    editの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,14 @@ class PostsController < ApplicationController
     @post.save
     redirect_to "/posts/#{@post.id}"
   end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+    redirect_to "/posts/#{@post.id}"
+  end
 
 private
 
