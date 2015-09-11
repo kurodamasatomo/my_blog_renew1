31d4ef9d0ca81a42d4cee3807d6f10af4de5e825
黒田 将智 <kurodamasatomo@kurox.local>
2015-09-11 21:04:43 +0900

    newの開発

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,24 @@ class PostsController < ApplicationController
   def show
     @post=Post.find(params[:id])
   end
+  def new
+    @post=Post.new
+  end
+  def  create
+    @post=Post.new(post_params)
+    @post.save
+    redirect_to "/posts/#{@post.id}"
+  end
 
-end
+private
+
+  def post_params
+    params.require(:post).permit(
+      :title,
+      :body,
+      :category
+      )
+
+  end
+
+  end
