533034647220a6515e3e2654141d5b9d7b4422bc
黒田 将智 <kurodamasatomo@kurox.local>
2015-09-11 17:39:02 +0900

    index開発

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create(title: "自習について",body: "いかに時間を作るか",category: "スパルタキャンプ")
+Post.create(title: "ブログアプリの復習",body: "CRUD機能からまずは開発する",category: "スパルタキャンプ")
